fbb5c2ea549b4e2a797ab03221c0466c45c52905
Pawan Sasanka <pawan@duosoftware.com>
2016-07-25 10:20:10 +0530

    config updated

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -26,15 +26,6 @@ module.exports = {
 
     },
 
-    "Kamailio":
-    {
-        "User":"SYS_LBDATABASE_MYSQL_USER",
-        "Password":"SYS_LBDATABASE_MYSQL_PASSWORD",
-        "Host":"SYS_LBDATABASE_HOST",
-        "Port":"SYS_LBMYSQL_PORT",
-        "Database":"SYS_LBDATABASE_MYSQL_DB"
-    },
-
     "Host":
     {
         "domain": "HOST_NAME",
